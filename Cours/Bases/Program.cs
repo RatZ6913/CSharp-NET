07a2386fa60825f826a28589971d6de97f80a349
Rattana <rattana.pheak.rp@gmail.com>
2023-08-18 13:19:01 +0200

    Interpretation des  chaînes
    Interpretation des  chaînes

--- Cours/Bases/Program.cs
+++ Cours/Bases/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Bonjour ! Comment t'appeles-tu ?");
+string? answer = Console.ReadLine();
 
+Console.WriteLine("Bonjour " + answer + " !");
+
+Console.WriteLine("Quelle est ton année de naissance ?");
+answer = Console.ReadLine();
+
+int dateBirth = int.Parse(answer);
+
+int age = DateTime.Today.Year - dateBirth;
+
+Console.WriteLine($"Tu vas avoir {age} ans en {DateTime.Today.Year}");
